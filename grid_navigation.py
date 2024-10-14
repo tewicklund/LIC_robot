7c39b241326137ae8dceb3fcbba34c2c7d3532fd
tewicklund <tewicklund@ucdavis.edu>
2024-10-14 11:10:40 -0700

    trying full instruction list

--- grid_navigation.py
+++ grid_navigation.py
@@ -29,8 +29,8 @@ turn_num=0
 stop_time=2
 
 # list of instructions, 'S' means stop at the line, and 'T' means turn at the line followed by the direction and the target magnetometer reading
-#instruction_list=['S','S','S','S','S','L','S','L','S','S','S','S','L','S','L']
-instruction_list=['S','S','L']
+instruction_list=['S','S','S','S','S','L','S','L','S','S','S','S','L','S','L']
+#instruction_list=['S','S','L']
 arm_position_list=['a','b',"c"]
 # stop_list=[6,2,5,2,5]
 # turn_list=["L","L","R","R","L"]
