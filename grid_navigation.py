0f3ccfbf1249bbcb2829974b1280f57788d7a67c
tewicklund <tewicklund@ucdavis.edu>
2024-10-14 10:01:00 -0700

    adjust timing

--- grid_navigation.py
+++ grid_navigation.py
@@ -158,9 +158,9 @@ try:
             time.sleep(stop_time/2)
 
             if instruction_list[stop_num]=='R':
-                right_turn(1.5,i2c_bus)
+                right_turn(1.4,i2c_bus)
             elif instruction_list[stop_num]=='L':
-                left_turn(1.4,i2c_bus)
+                left_turn(1.2,i2c_bus)
 
             move_arm(arm_position_list[stop_num%3],i2c_bus)
             move_arm('z',i2c_bus)
