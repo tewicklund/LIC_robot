cc52e0fb66878034c8827d8c2c72d61ec27f69c8
tewicklund <tewicklund@ucdavis.edu>
2024-11-01 13:10:38 -0700

    added servo driver test

---
+++ PCA9685_register_write.py
@@ -0,0 +1,30 @@
+import smbus
+import time
+
+bus = smbus.SMBus(7)  # Use 1 for i2c-1
+
+
+device_address = 0x40  
+prescale_address=0xfe
+prescale_value=0x12
+
+led_0_address_ls=0x06
+led_0_address_ms=0x07
+
+# values to write to led 0 as a test, expect 6.25% duty cycle
+led_0_value_ls=0xff
+led_0_value_ms=0x00
+
+
+# Write byte to prescale address to set frequency to 330 Hz
+bus.write(device_address,prescale_address,prescale_value)
+time.sleep(0.01)
+
+# write bytes to led 0 addresses
+bus.write(device_address,led_0_address_ls,led_0_value_ls)
+time.sleep(0.01)
+bus.write(device_address,led_0_address_ms,led_0_value_ms)
+time.sleep(0.01)
+
+# Close the bus connection if done
+bus.close()
