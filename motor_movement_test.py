fc11f0b9fb187385864cd391ae250672f6e76464
tewicklund <tewicklund@ucdavis.edu>
2024-11-04 15:15:56 -0800

    adjusted motor driver code again

--- motor_movement_test.py
+++ motor_movement_test.py
@@ -3,14 +3,11 @@ from functions import *
 
 i2c_bus = smbus2.SMBus(7)
 
-speed=int(input())
+speed=int(input("Enter speed, from 0 to 63"))
 
-for x in range(100):
-    drive_motor_exp('R',1,i2c_bus)
-    drive_motor_exp('L',1,i2c_bus)
-    time.sleep(0.01)
+while True:
+    for x in range(100):
+        drive_motor_exp('R',speed,i2c_bus)
+        drive_motor_exp('L',speed,i2c_bus)
+        time.sleep(0.01)
 
-for x in range(100):
-    drive_motor_exp('R',63,i2c_bus)
-    drive_motor_exp('L',63,i2c_bus)
-    time.sleep(0.01)
