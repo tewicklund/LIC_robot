3ba7ee833736233b2b8276fed07acf29224fb169
tewicklund <tewicklund@ucdavis.edu>
2024-11-12 13:48:27 -0800

    added directions

--- gyro_turn.py
+++ gyro_turn.py
@@ -28,46 +28,87 @@ current_speed=0
 base_speed=20
 min_speed=5
 p=12.5
+direction='L'
 
 try:
-    while radians_turned<target_radians:
-        # Get frameset of motion data
-        frames = pipeline.wait_for_frames()
+    if direction=="R":
+        while radians_turned<target_radians:
+            # Get frameset of motion data
+            frames = pipeline.wait_for_frames()
 
-        # Get gyroscope data
-        gyro_frame = frames.first_or_default(rs.stream.gyro)
-        
-
-        if gyro_frame:
-            # Extract gyroscope data (x, y, z)
-            gyro_data = gyro_frame.as_motion_frame().get_motion_data()
-            gx, gy, gz = gyro_data.x, gyro_data.y, gyro_data.z
-
-            # Print the accelerometer and gyroscope values
-            # print(f"Gyroscope: x={gx:.3f}, y={gy:.3f}, z={gz:.3f}")
-
-            # update the amount of radians turned so far
-            time_elapsed=time.time()-timestamp
-            timestamp=time.time()
-            radians_turned+=gz*time_elapsed
-            #print(f"Radians turned: {radians_turned:.3f}")
-
-            #adjust motor speeds based on radians turned
-            error=target_radians-radians_turned
-            motor_speed=error*p
+            # Get gyroscope data
+            gyro_frame = frames.first_or_default(rs.stream.gyro)
             
-            #make sure motor speed doesn't drop too low
-            motor_speed=clamp(motor_speed,min_speed,base_speed)
+
+            if gyro_frame:
+                # Extract gyroscope data (x, y, z)
+                gyro_data = gyro_frame.as_motion_frame().get_motion_data()
+                gx, gy, gz = gyro_data.x, gyro_data.y, gyro_data.z
+
+                # Print the accelerometer and gyroscope values
+                # print(f"Gyroscope: x={gx:.3f}, y={gy:.3f}, z={gz:.3f}")
+
+                # update the amount of radians turned so far
+                time_elapsed=time.time()-timestamp
+                timestamp=time.time()
+                radians_turned+=gz*time_elapsed
+                #print(f"Radians turned: {radians_turned:.3f}")
+
+                #adjust motor speeds based on radians turned
+                error=target_radians-radians_turned
+                motor_speed=error*p
+                
+                #make sure motor speed doesn't drop too low
+                motor_speed=clamp(motor_speed,min_speed,base_speed)
+                
+                #drive motors to make the turn
+                drive_motor_exp('L',motor_speed,i2c_bus)
+                drive_motor_exp('R',-motor_speed,i2c_bus)
             
-            #drive motors to make the turn
-            drive_motor_exp('L',motor_speed,i2c_bus)
-            drive_motor_exp('R',-motor_speed,i2c_bus)
+            # Delay to reduce CPU load
+            # time.sleep(0.1)
         
-        # Delay to reduce CPU load
-        # time.sleep(0.1)
-    
-    drive_motor_exp('L',0,i2c_bus)
-    drive_motor_exp('R',0,i2c_bus)
+        drive_motor_exp('L',0,i2c_bus)
+        drive_motor_exp('R',0,i2c_bus)
+    else:
+        while radians_turned>target_radians:
+            # Get frameset of motion data
+            frames = pipeline.wait_for_frames()
+
+            # Get gyroscope data
+            gyro_frame = frames.first_or_default(rs.stream.gyro)
+            
+
+            if gyro_frame:
+                # Extract gyroscope data (x, y, z)
+                gyro_data = gyro_frame.as_motion_frame().get_motion_data()
+                gx, gy, gz = gyro_data.x, gyro_data.y, gyro_data.z
+
+                # Print the accelerometer and gyroscope values
+                # print(f"Gyroscope: x={gx:.3f}, y={gy:.3f}, z={gz:.3f}")
+
+                # update the amount of radians turned so far
+                time_elapsed=time.time()-timestamp
+                timestamp=time.time()
+                radians_turned+=gz*time_elapsed
+                #print(f"Radians turned: {radians_turned:.3f}")
+
+                #adjust motor speeds based on radians turned
+                error=target_radians-radians_turned
+                motor_speed=error*p
+                
+                #make sure motor speed doesn't drop too low
+                motor_speed=clamp(motor_speed,min_speed,base_speed)
+                
+                #drive motors to make the turn
+                drive_motor_exp('L',motor_speed,i2c_bus)
+                drive_motor_exp('R',-motor_speed,i2c_bus)
+            
+            # Delay to reduce CPU load
+            # time.sleep(0.1)
+        
+        drive_motor_exp('L',0,i2c_bus)
+        drive_motor_exp('R',0,i2c_bus)
     
 except KeyboardInterrupt:
     print("Stopping data capture...")
