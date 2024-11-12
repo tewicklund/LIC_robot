347b96995583bb4d3e5d51366c4f608c4058dd64
tewicklund <tewicklund@ucdavis.edu>
2024-11-12 13:34:58 -0800

    added some proportional control

--- gyro_turn.py
+++ gyro_turn.py
@@ -5,54 +5,64 @@ from functions import *
 
 i2c_bus = smbus2.SMBus(7)
 
-
-
 # Initialize RealSense pipeline
 pipeline = rs.pipeline()
 config = rs.config()
 
 # Enable the motion sensor stream
-#config.enable_stream(rs.stream.accel)
 config.enable_stream(rs.stream.gyro)
 
 # Start streaming with the specified configuration
 pipeline.start(config)
 
+# timing used for integration
 timestamp=time.time()
+
+# variables to store angle position info
 radians_turned=0
+error=0
 target_radians=np.pi/2
+
+# speed settings for the motors
+current_speed=0
+base_speed=20
+min_speed=5
+p=12.5
 
 try:
     while radians_turned<target_radians:
         # Get frameset of motion data
         frames = pipeline.wait_for_frames()
 
-        # Get accelerometer and gyroscope data
-        #accel_frame = frames.first_or_default(rs.stream.accel)
+        # Get gyroscope data
         gyro_frame = frames.first_or_default(rs.stream.gyro)
+        
 
-        #spin motors
-        drive_motor_exp('L',20,i2c_bus)
-        drive_motor_exp('R',-20,i2c_bus)
-
-        if gyro_frame:# and accel_frame:
-            # Extract accelerometer data (x, y, z)
-            #accel_data = accel_frame.as_motion_frame().get_motion_data()
-            #ax, ay, az = accel_data.x, accel_data.y, accel_data.z
-
+        if gyro_frame:
             # Extract gyroscope data (x, y, z)
             gyro_data = gyro_frame.as_motion_frame().get_motion_data()
             gx, gy, gz = gyro_data.x, gyro_data.y, gyro_data.z
 
             # Print the accelerometer and gyroscope values
-            #print(f"Accelerometer: x={ax:.3f}, y={ay:.3f}, z={az:.3f}")
-            print(f"Gyroscope: x={gx:.3f}, y={gy:.3f}, z={gz:.3f}")
+            # print(f"Gyroscope: x={gx:.3f}, y={gy:.3f}, z={gz:.3f}")
 
+            # update the amount of radians turned so far
             time_elapsed=time.time()-timestamp
             timestamp=time.time()
             radians_turned+=gz*time_elapsed
-            print(f"Radians turned: {radians_turned:.3f}")
+            #print(f"Radians turned: {radians_turned:.3f}")
 
+            #adjust motor speeds based on radians turned
+            error=target_radians-radians_turned
+            motor_speed=error*p
+            
+            #make sure motor speed doesn't drop too low
+            if motor_speed<min_speed:
+                motor_speed=min_speed
+            
+            #drive motors to make the turn
+            drive_motor_exp('L',motor_speed,i2c_bus)
+            drive_motor_exp('R',-motor_speed,i2c_bus)
         
         # Delay to reduce CPU load
         # time.sleep(0.1)
