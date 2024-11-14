0390ec7f8adcd576acddecc4134e2c8c79626046
tewicklund <tewicklund@ucdavis.edu>
2024-11-14 13:54:29 -0800

    added arduino code for posting to http server

---
+++ arduino_stuff/All_LIC_POST/All_LIC_POST.ino
@@ -0,0 +1,150 @@
+#include <WiFiS3.h>             // Library for WiFi on Arduino Uno Rev4
+#include <WiFiClient.h>         // Library for Arduino to act as a WiFi client
+#include <ArduinoHttpClient.h>  // Library for Arduino to act as an HTTP client
+#include <WiFiUdp.h>            // UDP library for network communication
+#include <Wire.h>               // I2C library
+#include "Adafruit_TCS34725.h"  // Library for color sensor
+
+// Number to specify UUT, THIS WILL NEED TO BE ADJUSTED WHEN CHANGING UUT
+const int SENSOR_ID = 4;
+
+// Information about the WiFi network the Arduino needs to join
+const char* SSID = "Ynet";
+const char* PASSWORD = "CLTC1234";
+
+// Information about the HTTP server that will receive and store LIC trigger events
+const char* SERVER = "192.168.4.6";  // e.g., "example.com"
+const int PORT = 8080;               // Change to 443 for HTTPS
+const char* PATH = "/LIC_triggers";
+
+// Information about the ntp server used to get an epoch timestamp UTC
+const char* ntpServer = "pool.ntp.org";
+const int ntpPort = 123;                          // NTP uses port 123
+const unsigned long seventyYears = 2208988800UL;  // UNIX epoch starts from 1970, NTP from 1900
+const int timeZoneOffset = 0;                     // Offset in seconds, adjust based on your timezone
+
+// Digital pin assignment
+const int buttonPin = 2;    // connect test button to digital pin 2
+const int presencePin = 3;  // connect TTL output from LIC to digital pin 3 (Keilton only)
+
+// WiFi setup stuff
+WiFiClient wifiClient;
+HttpClient client = HttpClient(wifiClient, SERVER, PORT);
+WiFiUDP udp;
+
+// Color sensor setup
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_1X);
+uint16_t r, g, b, c, colorTemp;
+
+//analog read setup
+int analogReading=0;
+
+void setup() {
+  // Serial comms setup
+  Serial.begin(115200);
+
+  // Input pin setup
+  pinMode(buttonPin, INPUT);
+  pinMode(presencePin, INPUT);
+
+  //WiFi setup
+  if (WiFi.begin(SSID, PASSWORD) == WL_CONNECTED) {
+    Serial.println("Connected to WiFi");
+  } else {
+    Serial.println("Failed to connect to WiFi");
+    while (true)
+      ;
+  }
+  udp.begin(ntpPort);
+
+  // Color sensor setup, only applicable for 2 LICs
+  if (SENSOR_ID == 3 || SENSOR_ID == 4) {
+    if (tcs.begin()) {
+      Serial.println("Found sensor");
+    } else {
+      Serial.println("No TCS34725 found ... check your connections");
+      while (1)
+        ;
+    }
+  }
+}
+
+void loop() {
+  switch (SENSOR_ID) {
+
+    // test mode, use button to trigger POST request
+    case 0:
+      if (digitalRead(buttonPin)) {
+        Serial.println("Detected button press, sending post request");
+        sendPostRequest();
+        Serial.println("Sent request successfully! Waiting 5 seconds...");
+        delay(5000);
+        Serial.println("Waiting for next button press...");
+      }
+      break;
+
+    // UUT = Keilton LIC
+    case 1:
+      if (digitalRead(presencePin)) {
+        Serial.println("Detected presence from Keilton LIC, sending post request");
+        sendPostRequest();
+        Serial.println("Sent request successfully! Waiting 5 seconds...");
+        delay(5000);
+        Serial.println("Waiting for next LIC trigger...");
+      }
+      break;
+
+    // UUT = Acuity LIC
+    case 2:
+      analogReading=analogRead(A0);
+      if (analogReading < 512) {
+        Serial.print("Detected presence from Acuity LIC (analogRead = ");
+        Serial.print(analogReading);
+        Serial.println("), sending post request");
+        sendPostRequest();
+        Serial.println("Sent request successfully! Waiting 5 seconds...");
+        delay(5000);
+        Serial.println("Waiting for next LIC trigger...");
+      }
+      break;
+
+    // UUT = Cooper LIC
+    case 3:
+      tcs.getRawData(&r, &g, &b, &c);
+      if (b > 160) {
+        Serial.print("Detected presence from Cooper LIC (Blue = ");
+        Serial.print(b);
+        Serial.println("), sending post request");
+        sendPostRequest();
+        Serial.println("Sent request successfully! Waiting 5 seconds...");
+        delay(5000);
+        Serial.println("Waiting for next LIC trigger...");
+      }
+      break;
+
+    //UUT = EasySense LIC, the one that can't be taken out of its luminaire
+    case 4:
+      tcs.getRawData(&r, &g, &b, &c);
+      if (r > 40) {
+        Serial.print("Detected presence from EasySense LIC (Red = ");
+        Serial.print(r);
+        Serial.println("),sending post request");
+        sendPostRequest();
+        Serial.println("Sent request successfully! Waiting 5 seconds...");
+        delay(5000);
+        Serial.println("Waiting for next LIC trigger...");
+      }
+      break;
+
+    //UUT = WIZ20 LIC
+    case 5:
+      if (digitalRead(presencePin)) {
+        Serial.println("Detected presence from WIZ20 LIC, sending post request");
+        sendPostRequest();
+        Serial.println("Sent request successfully! Waiting 5 seconds...");
+        delay(5000);
+        Serial.println("Waiting for next LIC trigger...");
+      }
+      break;
+  }
+}
---
+++ arduino_stuff/All_LIC_POST/functions.ino
@@ -0,0 +1,53 @@
+unsigned long getEpochTime() {
+  // Send an NTP packet to the time server
+  sendNTPpacket(ntpServer);
+  
+  // Wait for a response for up to 1 second
+  delay(1000);
+  if (udp.parsePacket()) {
+    byte buffer[48];
+    udp.read(buffer, 48);
+
+    // Extract time in seconds since Jan 1, 1900 (NTP epoch)
+    unsigned long highWord = word(buffer[40], buffer[41]);
+    unsigned long lowWord = word(buffer[42], buffer[43]);
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+
+    // Convert NTP time to UNIX epoch
+    unsigned long epochTime = secsSince1900 - seventyYears + timeZoneOffset;
+    return epochTime;
+  }
+  return 0; // Return 0 if no response
+}
+
+void sendNTPpacket(const char* address) {
+  // Clear the buffer and prepare the request packet
+  byte packetBuffer[48] = { 0 };
+  packetBuffer[0] = 0b11100011; // NTP request header
+  
+  // Send the packet
+  udp.beginPacket(address, ntpPort);
+  udp.write(packetBuffer, 48);
+  udp.endPacket();
+}
+
+void sendPostRequest() {
+  unsigned long timestamp = getEpochTime();
+  String payload = "{\"sensor_id\":" + String(SENSOR_ID) + ", \"timestamp\":" + String(timestamp) + "}";
+
+  client.beginRequest();
+  client.post(PATH);
+  client.sendHeader("Content-Type", "application/json");
+  client.sendHeader("Content-Length", payload.length());
+  client.beginBody();
+  client.print(payload);
+  client.endRequest();
+
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+}
