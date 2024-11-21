e8cd581e6b6eeb61ef172d56844ff1701a068753
tewicklund <tewicklund@ucdavis.edu>
2024-11-21 15:24:17 -0800

    raised ratio limit

--- qr_grid_navigation.py
+++ qr_grid_navigation.py
@@ -26,7 +26,7 @@ x_location_error_sum=0
 x_location_error_max=400
 
 # minimum ratio of horizontal lines to vertical lines that is recognized as a stop
-ratio_limit=0.8
+ratio_limit=1.6
 
 # boolean for loop logic, used for driving over previously acknowledged horizontal lines
 horizontal_lines_acknowledged=False
